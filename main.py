1f27a48c3cd5983ef0f31e5b3b3b1b57c8743061
Amir mahdi Rezae Tavana <amirredfox117@gmail.com>
2022-06-08 08:46:59 -0700

    Make Snake Move
    Added While loop for our Game loop it's make snake Run
    Used Time to make Game sleep for 0.1 sec

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from turtle import Turtle, Screen
-
+import time
 positions = [(0, 0), (-20, 0), (-40, 0)]
 
 # Initialized Screen
@@ -7,12 +7,27 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 
+# Create Snake
 segments = []
 for seg in positions:
     segment = Turtle(shape="square")
     segment.color("white")
+    segment.penup()
     segment.goto(seg)
+    segments.append(segment)
 
+
+Game_Is_on = True
+
+while Game_Is_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg in range(len(segments)-1, 0, -1):
+        new_x = segments[seg-1].xcor()
+        new_y = segments[seg-1].ycor()
+        segments[seg].goto(new_x, new_y)
+    segments[0].forward(20)
 screen.exitonclick()
