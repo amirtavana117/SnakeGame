81ad4fcf68fe34dc9774f3b5977dd21c165d11e6
amirtavana117 <amirmahdirezaetavana@gmail.com>
2022-07-16 15:03:09 +0430

    Add File To the Game That Recored the highst score

--- ScoreBoard.py
+++ ScoreBoard.py
@@ -5,13 +5,22 @@ from turtle import Turtle
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
-        self.highscore = 0
+        self.highscore = self.ReadFile()
         self.goto(0, 255)
         self.color("white")
         self.penup()
         self.score = 0
         self.hideturtle()
         self.update_scoreboard()
+
+    def ReadFile(self):
+        with open("score.txt") as file:
+            score_ = int(file.read())
+        return score_
+
+    def Write_file(self, score):
+        with open("score.txt", 'w') as file:
+            file.write(str(score))
 
     def update_scoreboard(self):
         self.clear()
@@ -21,6 +30,7 @@ class ScoreBoard(Turtle):
     def reset_scoreBoard(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            self.Write_file(self.score)
         self.score = 0
         self.update_scoreboard()
 
